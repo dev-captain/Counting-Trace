669d24626d46af0516159e2e92b25890cb226ffd
dev-captain <luckydeveloper1113@gmail.com>
2024-07-16 12:16:08 +0900

    Update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,21 +1,22 @@
 const Navbar = () => {
   return(
-    <header className="z-10 w-full items-center justify-between text-sm lg:flex">
+    <header className="z-10 w-full items-center justify-center text-sm lg:flex">
       <div className="flex gap-x-10">
         <h1 className="">IWA COUNTING TRACE</h1>
+
+      </div>
+    <div
+        className="fixed bottom-0 right-0 flex h-48 w-full items-center justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
         <p>account:Taro Yamada</p>
-      </div>
-      <div
-        className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
         <a
-          className="pointer-events-none flex place-items-center gap-2 p-2 lg:pointer-events-auto rounded-md bg-[#2380C8]"
-          href=""
-          target="_blank"
-          rel="noopener noreferrer"
+            className="pointer-events-none flex place-items-center gap-2 p-2 lg:pointer-events-auto rounded-md bg-[#2380C8]"
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
         >
-          LOGOUT
+            LOGOUT
         </a>
-      </div>
+    </div>
     </header>
   )
 }
