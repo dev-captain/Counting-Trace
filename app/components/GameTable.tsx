2f7a1b92d6f8e4bc741983cd7f7ee64e5d2a48e5
dev-captain <luckydeveloper1113@gmail.com>
2024-07-18 17:26:19 +0900

    タブのデザイン修正

--- app/components/GameTable.tsx
+++ app/components/GameTable.tsx
@@ -10,21 +10,19 @@ export default function GameTable() {
     return (
         <div className="min-h-screen">
             <div className="scrollable">
-                <div className="border-b">
-                    <div className="flex">
-                        <button
-                            className={`px-4 py-2 text-sm font-medium text-gray-700 ${currentTab === 'Baccarat' ? 'bg-[#BECAD6]' : 'bg-gray-200'}`}
-                            onClick={() => setCurrentTab('Baccarat')}
-                        >
-                            Baccarat
-                        </button>
-                        <button
-                            className={`px-4 py-2 text-sm font-medium text-gray-700 ${currentTab === 'Blackjack' ? 'bg-[#BECAD6]' : 'bg-gray-200'}`}
-                            onClick={() => setCurrentTab('Blackjack')}
-                        >
-                            Blackjack
-                        </button>
-                    </div>
+                <div className="flex gap-x-2">
+                    <button
+                        className={`px-4 py-2 text-sm font-medium text-gray-700 rounded-t-lg ${currentTab === 'Baccarat' ? 'bg-[#BECAD6]' : 'bg-gray-200'}`}
+                        onClick={() => setCurrentTab('Baccarat')}
+                    >
+                        Baccarat
+                    </button>
+                    <button
+                        className={`px-4 py-2 text-sm font-medium text-gray-700 rounded-t-lg ${currentTab === 'Blackjack' ? 'bg-[#BECAD6]' : 'bg-gray-200'}`}
+                        onClick={() => setCurrentTab('Blackjack')}
+                    >
+                        Blackjack
+                    </button>
                 </div>
                 <div className="bg-[#BECAD6] p-4">
                     {currentTab === 'Baccarat' ? <BaccaratTable/> : <BlackjackTable/>}
